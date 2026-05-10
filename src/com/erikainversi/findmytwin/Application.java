c9bfd8f7865c9ef30f05aef9c6147bdfbe78009a
Erika <erika.inversi@gmail.com>
2026-05-10 08:46:05 +0200

    Define main application workflow

--- src/com/erikainversi/findmytwin/Application.java
+++ src/com/erikainversi/findmytwin/Application.java
@@ -4,6 +4,19 @@ public class Application {
 
 	public static void main(String[] args) {
 
+		// 1. Read folder path from user
+
+		// 2. Scan folders and subfolders
+
+		// 3. Find image files
+
+		// 4. Calculate file hashes
+
+		// 5. Group identical files
+
+		// 6. Print groups of identical images
+		// including file names and file paths
+
 	}
 
 }
