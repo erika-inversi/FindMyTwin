5e1bf83a2ad8725a00f298c6902765338efa18fe
Erika <erika.inversi@gmail.com>
2026-05-24 09:44:49 +0200

    Implement ImagePathFilter with extension-based filtering

--- src/com/erikainversi/findmytwin/ImagePathFilter.java
+++ src/com/erikainversi/findmytwin/ImagePathFilter.java
@@ -1,21 +1,38 @@
 package com.erikainversi.findmytwin;
 
 import java.nio.file.Path;
+import java.util.Set;
 
 public class ImagePathFilter implements PathFilter {
+
+	private final Set<String> IMAGE_EXTENSIONS = Set.of(".png", ".jpg", ".jpeg", ".gif", ".webp", ".bmp", ".tiff",
+			".tif", ".svg", ".ico");
 
 	@Override
 	public boolean accept(Path path) {
 
+		if (path == null) {
+			return false;
+		}
+
 		Path fileName = path.getFileName();
+
+		if (fileName == null) {
+			return false;
+		}
 
 		String name = fileName.toString();
 
-		if (name.endsWith(".png") || name.endsWith(".jpg")) {
+		int indexLastDot = name.lastIndexOf('.');
 
+		if (indexLastDot == -1) {
+			return false;
 		}
 
-		return true;
+		String extension = name.substring(indexLastDot);
+
+		return IMAGE_EXTENSIONS.contains(extension);
+
 	}
 
 }
